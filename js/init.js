a7197ad8959d8f88e5a17ba8ebee1d30d7282b6d
Nuno Carvalho <nuno.carvalho@cm-guimaraes.pt>
2022-03-25 14:17:51 +0000

    Add social media link and auto last updated

--- js/init.js
+++ js/init.js
@@ -2,4 +2,9 @@
 document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.sidenav')
 	var instances = M.Sidenav.init(elems, {})
+
+	const time = new File([],'index.html').lastModified,
+	      date = new Date(time)
+
+	document.getElementById('modified').innerHTML = `${ date.getDate() }-${ date.getMonth()+1 }-${ date.getFullYear() }`
 })

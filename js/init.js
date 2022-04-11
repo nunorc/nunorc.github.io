5231045e39fd6d1fc5a19ce85c3ebcccadbf04f6
Nuno Carvalho <narcarvalho@gmail.com>
2022-04-11 09:03:02 +0100

    Improve auto last updated

--- js/init.js
+++ js/init.js
@@ -3,8 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.sidenav')
 	var instances = M.Sidenav.init(elems, {})
 
-	const time = new File([],'index.html').lastModified,
-	      date = new Date(time)
-
-	document.getElementById('modified').innerHTML = `${ date.getDate() }-${ date.getMonth()+1 }-${ date.getFullYear() }`
+	fetch('https://api.github.com/repos/nunorc/nunorc.github.io')
+	  .then(response => response.json())
+	  .then(data => {
+	  	console.log(data)
+	  	document.getElementById('modified').innerHTML = data['pushed_at'].split('T')[0]
+	  })
 })
